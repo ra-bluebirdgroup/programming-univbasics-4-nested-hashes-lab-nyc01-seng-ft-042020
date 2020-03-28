6d49223497ed6e56cc1bf43463a0fa23b3e407da
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-28 19:50:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,6 +38,8 @@ def alan_kay_is_known_for
       }
     }
 
+	programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -56,7 +58,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+ programmer_hash[:dennis_ritchie][:known_for]
 end
 
 def adding_matz
