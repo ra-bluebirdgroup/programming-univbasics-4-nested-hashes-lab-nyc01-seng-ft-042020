afc78af090e9e41a70a163c0b1e0e22b18ef96af
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-28 20:14:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,13 +85,16 @@ def adding_matz
       }
     }
 
-programmer_hash < {
-			:yukihiro_matsumoto => {
-        :known_for => "Ruby",
-        :languages => ["LISP", "C"]
-       }
-     }
-programmer_hash
+
+programmer_hash[
+
+				:yukihiro_matsumoto => {
+	        :known_for => "Ruby",
+	        :languages => ["LISP", "C"]
+	       }
+	     
+
+]
 end
 
 def changing_alan
