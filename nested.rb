6ca93932ea330507eac3441dce88b7a8dbe7d413
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-28 20:21:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,14 +85,10 @@ def adding_matz
       }
     }
 
-
 programmer_hash[:yukihiro_matsumoto] = {
        :known_for => "Ruby",
        :languages => ["LISP", "C"]
-       }
-
-
-]
+       } 
 programmer_hash
 end
 
